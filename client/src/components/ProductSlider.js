5f0e11aec87fd07a3f66089a5e85e87e0afd7e0b
Ricardo Côrtes <giralander@hotmail.com>
2023-05-15 15:46:55 +0200

    Added slider

--- client/src/components/ProductSlider.js
+++ client/src/components/ProductSlider.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css'
+import 'swiper/css/pagination'
+import { Pagination, Navigation } from 'swiper';
 
-const ProductSlider = () => {
-  return <div>ProductSlider</div>;
+import Product from '../components/Product'
+
+
+const ProductSlider = ({ data }) => {
+  // console.log(data)
+  return <Swiper>
+    {data.map((product) => {
+      return <SwiperSlide>slide</SwiperSlide>
+    })}
+  </Swiper>;
 };
 
 export default ProductSlider;
