d220de4e241a250ea8736c99d451c06b72065542
Ricardo Côrtes <giralander@hotmail.com>
2023-05-15 14:06:16 +0200

    initial

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+// pages
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
+import Search from './pages/Search';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/products/:id" element={<Layout><Products /></Layout>} />
+        <Route path="/product/:id" element={<Layout><ProductDetails /></Layout>} />
+        <Route path="/search" element={<Layout><Search /></Layout>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
---
+++ client/src/components/SearchForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const SearchForm = () => {
+  return (
+    // <form onSubmit={handleSubmit}
+    // className= { "${
+    // isAnimating? 'animate-shake': 'animate-none'
+    // } w-full relative }
+    // <input
+    // onChange = {handleSearch Input}
+    // className='input'
+    // type='text'
+    // placeholder='Search for a product...' 
+    // />
+
+    // <button className='btn btn-accent absolute top-right-rounded-t1-none
+    // rounded-bl-none'>
+
+    // {/* <FiSearch className='text-x1' /> */}
+    // </button>
+    // </form>
+    <div>form</div>
+  )
+};
+
+export default SearchForm;
