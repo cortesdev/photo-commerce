282be43ee84d7344b36de89ad0927cd2714febcd
Ricardo Côrtes <giralander@hotmail.com>
2023-05-15 15:26:11 +0200

    API INIT

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,13 @@
 import React from 'react';
+// useFetch Home
 
+import useFetch from '../hooks/useFetch'
 const Home = () => {
+  const { data } = useFetch('/products?populate=*')
+
+
+  console.log('DATA', data)
+
   return <div>Home</div>;
 };
 
