3d0c40a08bf67700bc60ba5419eaef0e9ebded68
Ricardo Côrtes <giralander@hotmail.com>
2023-05-15 15:40:00 +0200

    Commerce

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
 // useFetch Home
-
 import useFetch from '../hooks/useFetch'
+import ProductSlider from '../components/ProductSlider'
+
+
 const Home = () => {
-  const { data } = useFetch('/products?populate=*')
 
+  const { data } = useFetch('/products?populate=*&filters[isNew]=true')
+  // console.log('DATA', data)
 
-  console.log('DATA', data)
+  return <div className='mb-16'>
+    Home
 
-  return <div>Home</div>;
+    <div className='container mx-auto'>
+      <h2 className='m2 mb-6 text-center x1:text-left'>Latest Products</h2>
+    </div>
+
+    <ProductSlider data={data} />
+  </div>;
 };
 
 export default Home;
