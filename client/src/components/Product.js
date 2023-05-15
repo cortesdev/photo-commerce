dbfbb48527496373e59a82acd894f966545f2ad5
Ricardo Côrtes <giralander@hotmail.com>
2023-05-15 17:39:59 +0200

    Added product to slider and styles

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../slider.css'
 
 const Product = ({ product }) => {
   console.log(product)
   return (
     <Link>
-      <div>
-        <img
-          src={`http://localhost:1337${product.attributes.image.data.attributes.url}`}
-          alt=""
-          class=""
-        />
+      <div className='grad w-full rounded-[8px] overflow-hidden h-[362px] group'>
+        <div className='w-full h-[200px] flex items-center justify-center relative'>
+          <div className='absolute rounded-full z-10 text-[12px] px-2 right-2 top-2 bg-accent text-black'>New</div>
+          <img
+            src={`http://localhost:1337${product.attributes.image.data.attributes.url}`}
+            alt=""
+            class="w-[160px] h-[160px] group-hover:scale-90"
+          />
+        </div>
+
+        <div className='px-6 pb-8 flex flex-col'>
+          <div className="text-sm text-accent capitalize mb-2">
+            {product.attributes.categories.data[0].attributes.title}
+          </div>
+          <div className="text-[15px] mb-4 lg:mb-9">
+            {product.attributes.title.substring(0, 35)}...
+          </div>
+          <div className="text-lg text-accent">
+            €{product.attributes.price}
+          </div>
+
+        </div>
       </div>
     </Link>);
 };
--- client/src/components/ProductSlider.js
+++ client/src/components/ProductSlider.js
@@ -9,7 +9,30 @@ import Product from '../components/Product'
 
 const ProductSlider = ({ data }) => {
   // console.log(data)
-  return <Swiper>
+  return <Swiper modules={[Pagination, Navigation]} loop={false} navigation
+    pagination={{
+      clickable: true
+    }}
+    className='productSlider mx-auto max-w-[360px] md:max-w-lg xl:max-w-[1410px]'
+    breakpoints={{
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1440: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+      },
+    }}
+  >
     {data?.map((product) => {
       return <SwiperSlide key={product.id}>
         <Product product={product} />
