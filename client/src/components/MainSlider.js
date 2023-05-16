3725e866055b81c31d775d16f3457bfcc6346774
Ricardo Côrtes <giralander@hotmail.com>
2023-05-16 11:51:19 +0200

    Main slider content and styles

--- client/src/components/MainSlider.js
+++ client/src/components/MainSlider.js
@@ -1,9 +1,74 @@
 import React from 'react';
 import LatestProducts from './LatestProducts';
+import CameraImg from '../img/camera.png'
+import { SwiperSlide, Swiper } from 'swiper/react';
+import Product from './Product';
+import { Pagination, Navigation } from 'swiper';
+
+// data
+const sliderData = [
+  {
+    img: CameraImg,
+    pretitle: 'Special offer',
+    titlePart1: 'Save 20%',
+    titlePart2:
+      'On your',
+    titlePart3: 'first order',
+    btnText: 'Shop now'
+  },
+  {
+    img: CameraImg,
+    pretitle: 'Special offer',
+    titlePart1: 'Save 20%',
+    titlePart2: 'On your',
+    titlePart3: 'first order',
+    btnText: 'Shop now',
+  }];
 
 const MainSlider = () => {
-  return <div>
-    MainSlider  </div>;
+  return <Swiper modules={[Pagination]}
+    loop={true}
+    pagination={{
+      clickable: true
+    }}
+    className='mainSlider  mx-auto max-w-[360px] md:max-w-lg xl:max-w-[1410px] h-full bg-primary xl:bg-mainSlider xl:bg-no-repeat 
+      max-w-lg lg:max-w-none rounder-[8px] overflow-hidden drop-shadow-2xl'
+  >
+    {sliderData?.map((slide, index) => {
+      return (
+        <SwiperSlide key={index}>
+          <div className='flex flex-col lg:flex-row h-full p-[20px] md:p-[60px]'>
+            <div className='w-full lg:flex-1'>
+
+              <div className='mb-1 lg:text-left   uppercase
+              '>
+                {slide.pretitle}
+              </div>
+
+              <div className='text-3xl md:text-[46px] font-semibold uppercase
+             leading-none text-center lg:text-left mb-8 xl:mb-20'>
+                {slide.titlePart1} <br />
+
+                {slide.titlePart2}<br />
+
+                {slide.titlePart3}
+              </div>
+
+              <div className='flex-1'>
+                <img
+                  className='xl:absolute xl:-right-16 xl:-bottom-12'
+                  src={slide.img}
+                  alt='camera' />
+              </div>
+            </div>
+          </div>
+
+        </SwiperSlide>
+      )
+    })}
+
+  </Swiper>
+    ;
 };
 
 export default MainSlider;
